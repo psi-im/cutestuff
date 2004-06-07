2c7ebe64b1ad5e46f5d9c06017a203a4fbfe3f76
Justin Karneges <justin@affinix.com>
2004-06-07 06:49:40 +0000

    don't waste cpu when shutting down
    git-svn-id: file:///home/justin/tmp/tmp/svn/cutestuff/trunk@77 65d0b1b9-db85-4114-8a31-8ae23c1c3140

--- network/ndns.cpp
+++ network/ndns.cpp
@@ -53,6 +53,7 @@
 #include<qapplication.h>
 #include<qsocketdevice.h>
 #include<qptrlist.h>
+#include<qeventloop.h>
 
 #ifdef Q_OS_UNIX
 #include<netdb.h>
@@ -240,8 +241,10 @@ void NDnsManager::tryDestroy()
 
 void NDnsManager::app_aboutToQuit()
 {
-	while(man)
-		qApp->processEvents();
+	while(man) {
+		QEventLoop *e = qApp->eventLoop();
+		e->processEvents(QEventLoop::WaitForMore);
+	}
 }
 
 
